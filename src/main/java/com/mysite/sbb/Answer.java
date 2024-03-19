d12abef777026bdba098c47d2602f6df33ee88a9
yujin <sinyj827@naver.com>
2024-03-20 03:55:17 +0900

    2-5 commit

--- src/main/java/com/mysite/sbb/Answer.java
+++ src/main/java/com/mysite/sbb/Answer.java
@@ -1,24 +1,24 @@
-//package com.mysite.sbb;
-//
-//import jakarta.persistence.*;
-//import lombok.Getter;
-//import lombok.Setter;
-//
-//import java.time.LocalDateTime;
-//
-//@Getter
-//@Setter
-//@Entity
-//public class Answer {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Integer id;
-//
-//    @Column(columnDefinition = "TEXT")
-//    private String content;
-//
-//    private LocalDateTime createDate;
-//
-//    @ManyToOne
-//    private Question question;
-//}
+package com.mysite.sbb;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @ManyToOne
+    private Question question;
+}
